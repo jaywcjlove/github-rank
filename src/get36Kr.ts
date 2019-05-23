e322a8f4b15151b8168ac81e7e0c1f7cf2829553
jaywcjlove <398188662@qq.com>
2019-05-23 23:28:44 +0800

    Modify console log.

--- src/get36Kr.ts
+++ src/get36Kr.ts
@@ -8,8 +8,10 @@ import { saveData } from './utils/saveData';
     const today = new Date();
     const month = today.getMonth() + 1;
     const date = `${today.getFullYear()}-${month < 10 ? `0${month}` : month}-${today.getDate()}`;
-    data = data.filter((item: I36KrData) => date === item.created_at.split(' ')[0] && item.news_url); 
+    console.log(`> 共获取 36Kr 快讯 ${data.length} 条原始数据！`);
+    data = data.filter((item: I36KrData) => date === item.created_at.split(' ')[0] && item.news_url);
+    console.log(`> 共过滤 ${date} 36Kr 快讯 ${data.length} 条数据！`);
   }
   await saveData(data, '36kr.json');
-  console.log(`> 共获取 36Kr 快讯 ${data.length} 条数据！`);
+  // console.log(`> 共获取 36Kr 快讯 ${data.length} 条数据！`);
 })()
