4114c519ee6482c1af481cb043aa0faf8aec775f
jaywcjlove <398188662@qq.com>
2019-06-04 21:35:37 +0800

    Modify the filter data error issue.

--- src/get36Kr.ts
+++ src/get36Kr.ts
@@ -7,11 +7,14 @@ import { saveData } from './utils/saveData';
   if (data.length > 0) {
     const today = new Date();
     const month = today.getMonth() + 1;
-    const date = `${today.getFullYear()}-${month < 10 ? `0${month}` : month}-${today.getDate()}`;
-    console.log(`> 共获取 36Kr 快讯 ${data.length} 条原始数据！`);
+    const day = today.getDate();
+    const date = `${today.getFullYear()}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
+    console.log(`> 共获取 36Kr 快讯 ${data.length} 条原始数据！${date}`);
     data = data.filter((item: I36KrData) => date === item.created_at.split(' ')[0] && item.news_url);
     console.log(`> 共过滤 ${date} 36Kr 快讯 ${data.length} 条数据！`);
   }
-  await saveData(data, '36kr.json');
-  // console.log(`> 共获取 36Kr 快讯 ${data.length} 条数据！`);
+  if (data.length > 0) {
+    await saveData(data, '36kr.json');
+  }
+  console.log(`> 共获取 36Kr 快讯 ${data.length} 条数据！`);
 })()
