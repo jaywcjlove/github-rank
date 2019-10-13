a443bb1fb7ab9eee0e1d2f3ae2548803125e0f08
jaywcjlove <398188662@qq.com>
2019-10-13 22:17:24 +0800

    Update deploy script.

--- src/deploy.ts
+++ src/deploy.ts
@@ -44,7 +44,7 @@ const pkgPath = path.join(root, 'package.json');
     await execute('git push origin master');
     await execute('git pull --all');
     await execute('git branch --format "%(if)%(upstream:short)%(then)git push . %(upstream:short):%(refname:short)%(end)" | sh');
-    await execute('git push tee --all');
+    await execute('git push -f tee --all');
     (console as any)['100']('> done!');
   } catch(error) {
     console.log('error:', error);
