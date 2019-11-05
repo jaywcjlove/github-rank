81cbb2abbe1c2f9d16b1f3b2a633e65881faa10e
jaywcjlove <398188662@qq.com>
2019-11-05 10:37:48 +0800

    Modify the console log.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -106,7 +106,7 @@ export interface IResultReposData {
 export function getReposData(page: number): Promise<IResultReposData> {
   return fetch(`https://api.github.com/search/repositories?q=stars:>8000&page=${page}&per_page=100`)
     .then(res => {
-      console.log(`   Github API 获取用户计数: ${res.headers.get('x-ratelimit-limit')}/${res.headers.get('x-ratelimit-remaining')}`);
+      console.log(`   Github API 获取仓库Star排行计数: ${res.headers.get('x-ratelimit-limit')}/${res.headers.get('x-ratelimit-remaining')}`);
       console.log('   时间:', `${res.headers.get('x-ratelimit-reset')}000\n`);
       return res.json();
     });
