b5e3ed858584c1b968709e66c3ce086a216c69dc
jaywcjlove <398188662@qq.com>
2020-03-26 13:17:37 +0800

    fix(type): Fix type errors.

--- src/getUserInfo.ts
+++ src/getUserInfo.ts
@@ -8,14 +8,14 @@ import { saveUsersData } from './utils/saveUsersData';
 ;(async () => {
   try {
     // 获取【全球】用户数据排行榜
-    const users: IUserData[] = await saveUsersData(usersDist, usersCache, '');
+    const users: IUserData[] = await saveUsersData(usersDist as IUserData[], usersCache, '');
     if (usersCache.length === 0) {
       console.log(`-> 获取【全球】数据完成！${users.length}`);
     } else {
       console.log(`<- 获取【全球】用户数据未完成！还剩 ${usersCache.length} 个用户信息，待获取！`);
     }
     // 获取【中国】用户数据排行榜
-    const usersChina: IUserData[] = await saveUsersData(usersChinaDist, usersChinaCache, '.china', users);
+    const usersChina: IUserData[] = await saveUsersData(usersChinaDist as IUserData[], usersChinaCache as IUserData[], '.china', users);
     if (usersChinaCache.length === 0) {
       console.log(`-> 获取【中国】数据完成！${usersChina.length}`);
     } else {
